fc94d6088b74142dc4992fdde3a175d2bf0e225b
Will <yh2689@nyu.edu>
2023-04-12 20:12:39 +0400

    more experiments

--- code/utils/globals.py
+++ code/utils/globals.py
@@ -1,6 +1,8 @@
 # Data 
 MMLU_index_path = '../data/mmlu/MMLU_samples.json'
+DATA_DIR = '../data'
 
 # Outputs
 OUTPUT_DIR = '../results/outputs'
 AGGREGATED_OUTPUT_DIR = '../results/aggregated'
+FIGS_DIR = '../results/figs'
---
+++ code/utils/graph_utils.py
@@ -0,0 +1,12 @@
+import networkx
+import itertools
+
+def complete_graph_from_list(L, create_using=None):
+    G = networkx.empty_graph(len(L),create_using)
+    if len(L)>1:
+        if G.is_directed():
+            edges = itertools.permutations(L,2)
+        else:
+            edges = itertools.combinations(L,2)
+        G.add_edges_from(edges)
+    return G
